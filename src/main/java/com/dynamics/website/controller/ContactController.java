ceecde817012874a27efed8e8de88ab3a9fcfe51
akashdecoder <akash.ranjan1999@gmail.com>
2020-12-08 20:15:06 +0530

    Added contact us form

---
+++ src/main/java/com/dynamics/website/controller/ContactController.java
@@ -0,0 +1,52 @@
+package com.dynamics.website.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ContactController {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @GetMapping("/dynamics/contactus")
+    public String showContact() {
+        return "contactus.html";
+    }
+
+    @PostMapping("/dynamics/contactus")
+    public String sendMessage(HttpServletRequest request) {
+        String fullName = request.getParameter("fullName");
+        String email = request.getParameter("email");
+        String phone = request.getParameter("phone");
+        String message = request.getParameter("message");
+        System.out.println(email);
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        mailMessage.setFrom(email);
+        mailMessage.setTo("akashranjandas18cs@sit.ac.in");
+
+        String mailSubject = fullName +  " has sent a message";
+        String mailContent = "Sender name: " + fullName + "\n";
+        mailContent += "Sender E-mail: " + email + "\n";
+        mailContent += "Sender Phone: " + phone + "\n";
+        mailContent += "Content: " + message + "\n";
+
+        mailMessage.setSubject(mailSubject);
+        mailMessage.setText(mailContent);
+
+        mailSender.send(mailMessage);
+        System.out.println("Mail Sent");
+
+        return "message.html";
+
+
+    }
+}
--- src/main/java/com/dynamics/website/controller/PageController.java
+++ src/main/java/com/dynamics/website/controller/PageController.java
@@ -31,48 +31,9 @@ public class PageController {
         return "events.html";
     }
 
-//    @RequestMapping(value = "/dynamics/recruitment", method = RequestMethod.GET)
-//    public String viewRegister(Model model) {
-//
-//        AppUserForm form = new AppUserForm();
-//
-//        model.addAttribute("appUserForm", form);
-//
-//        return "recruitment.html";
-//    }
-//
-//    @RequestMapping(value = "/dynamics/recruitment", method = RequestMethod.POST)
-//    public String saveRegister(Model model, //
-//                               @ModelAttribute("appUserForm") @Validated AppUserForm appUserForm, //
-//                               BindingResult result, //
-//                               final RedirectAttributes redirectAttributes) {
-//
-//        // Validate result
-//        if (result.hasErrors()) {
-//            return "recruitment.html";
-//        }
-//        AppUser newUser= null;
-//        try {
-//            newUser = appUserDAO.createAppUser(appUserForm);
-//        }
-//        // Other error!!
-//        catch (Exception e) {
-//            model.addAttribute("errorMessage", "Error: " + e.getMessage());
-//            return "recruitment.html";
-//        }
-//
-//        redirectAttributes.addFlashAttribute("flashUser", newUser);
-//
-//        return "redirect:/dynamics";
-//    }
-
     @GetMapping("/dynamics/team")
     public String teamPage(){
         return "team.html";
     }
 
-    @GetMapping("/dynamics/contactus")
-    public String contactusPage(){
-        return "contactus.html";
-    }
 }
